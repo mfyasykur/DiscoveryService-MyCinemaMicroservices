905577b01cc59be719e29e816415be7e7e78f635
mfyasykur <mf.yasykur@gmail.com>
2022-12-04 09:22:42 +0700

    initial commit DiscoveryService of MyCinema Microservices

---
+++ src/test/java/edu/binar/challenge/MyCinemaMicroservices/DiscoveryService/DiscoveryServiceAppTests.java
@@ -0,0 +1,13 @@
+package edu.binar.challenge.MyCinemaMicroservices.DiscoveryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
